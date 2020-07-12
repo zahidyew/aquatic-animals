fdef7d831f9aa8a7694bfd7310f44b1d1597cf76
zahidyew <zahidyew96@gmail.com>
2020-07-13 02:48:58 +0800

    added facts overlay to scan marker

--- app/src/main/java/com/example/aquaticanimals/markerAR/AugmentedImageActivity.java
+++ app/src/main/java/com/example/aquaticanimals/markerAR/AugmentedImageActivity.java
@@ -4,19 +4,23 @@ import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.view.LayoutInflater;
 import android.view.PixelCopy;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.aquaticanimals.R;
-import com.example.aquaticanimals.utils.SnackbarHelper;
+import com.example.aquaticanimals.utils.Animal;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.ar.core.AugmentedImage;
 import com.google.ar.core.Frame;
 import com.google.ar.core.TrackingState;
@@ -34,40 +38,36 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * This application demonstrates using augmented images to place anchor nodes. app to include image
+/* This application demonstrates using augmented images to place anchor nodes. app to include image
  * tracking functionality.
  *
- * <p>In this example, we assume all images are static or moving slowly with a large occupation of
+ * Here, we assume all images are static or moving slowly with a large occupation of
  * the screen. If the target is actively moving, we recommend to check
  * ArAugmentedImage_getTrackingMethod() and render only when the tracking method equals to
- * AR_AUGMENTED_IMAGE_TRACKING_METHOD_FULL_TRACKING. See details in <a
- * href="https://developers.google.com/ar/develop/c/augmented-images/">Recognize and Augment
- * Images</a>.
- */
+ * AR_AUGMENTED_IMAGE_TRACKING_METHOD_FULL_TRACKING. . */
 public class AugmentedImageActivity extends AppCompatActivity {
 
   private ArFragment arFragment;
   private ImageView fitToScanView;
   private AugmentedImageNode node;
+  private ConstraintLayout mainLayout;
+  private FloatingActionButton infoBtn;
+  private String scannedAnimal;
 
-  // Augmented image and its associated center pose anchor, keyed by the augmented image in
-  // the database.
+  // Augmented image and its associated center pose anchor, keyed by the augmented image in the database.
   private final Map<AugmentedImage, AugmentedImageNode> augmentedImageMap = new HashMap<>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     setContentView(R.layout.activity_augmented_image);
-
-    // lock the screen to be in landscape orientation
-    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
+    mainLayout = findViewById(R.id.activity_augmented_image);
     arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
     fitToScanView = findViewById(R.id.image_view_fit_to_scan);
+    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
     arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
-
     setUpFloatingButton();
   }
 
@@ -82,16 +82,11 @@ public class AugmentedImageActivity extends AppCompatActivity {
   @Override
   protected void onDestroy() {
     super.onDestroy();
-    //arFragment.getArSceneView().destroy();
     arFragment.getArSceneView().getSession().close();
-    //arFragment.getArSceneView().destroyAllResources();
   }
 
-  /**
-   * Registered with the Sceneform Scene object, this method is called at the start of each frame.
-   *
-   * @param frameTime - time since last frame.
-   */
+  /* Registered with the Sceneform Scene object, this method is called at the start of each frame.
+     @param frameTime - time since last frame. */
   private void onUpdateFrame(FrameTime frameTime) {
     Frame frame = arFragment.getArSceneView().getArFrame();
 
@@ -108,8 +103,9 @@ public class AugmentedImageActivity extends AppCompatActivity {
         case PAUSED:
           // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
           // but not yet tracked.
-          String text = "Detected Image " + augmentedImage.getIndex();
-          SnackbarHelper.getInstance().showMessage(this, text);
+          String text = "Detected Image: " + augmentedImage.getName();
+          Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+          //SnackbarHelper.getInstance().showMessage(this, text);
           break;
 
         case TRACKING:
@@ -118,13 +114,14 @@ public class AugmentedImageActivity extends AppCompatActivity {
 
           // Create a new anchor for newly found images.
           if (!augmentedImageMap.containsKey(augmentedImage)) {
-            //text = "Detected Image " + augmentedImage.getName();
-            //SnackbarHelper.getInstance().showMessage(this, text);
             node = new AugmentedImageNode(this, augmentedImage.getName());
             Toast.makeText(getApplicationContext(), augmentedImage.getName(), Toast.LENGTH_SHORT).show();
             node.setImage(augmentedImage);
             augmentedImageMap.put(augmentedImage, node);
             arFragment.getArSceneView().getScene().addChild(node);
+
+            scannedAnimal = augmentedImage.getName();
+            infoBtn.setVisibility(View.VISIBLE);
           }
           break;
 
@@ -137,9 +134,50 @@ public class AugmentedImageActivity extends AppCompatActivity {
 
   public void setUpFloatingButton() {
     FloatingActionButton fab = findViewById(R.id.fab);
+    infoBtn = findViewById(R.id.infoBtn);
+    infoBtn.setVisibility(View.INVISIBLE);
+
     fab.setOnClickListener(view -> {
       takePhoto();
     });
+
+    infoBtn.setOnClickListener(view -> {
+      LayoutInflater inflater = AugmentedImageActivity.this.getLayoutInflater();
+      View animalFacts = inflater.inflate(R.layout.animal_facts, mainLayout, false);
+      mainLayout.addView(animalFacts);
+
+      FloatingActionButton exitBtn = animalFacts.findViewById(R.id.exitBtn);
+      setFacts(animalFacts);
+
+      exitBtn.setOnClickListener(view2 -> {
+        mainLayout.removeView(animalFacts);
+
+        infoBtn.setVisibility(View.VISIBLE);
+        fab.setVisibility(View.VISIBLE);
+      });
+
+      fab.setVisibility(View.INVISIBLE);
+      infoBtn.setVisibility(View.INVISIBLE);
+    });
+  }
+
+  private void setFacts(View animalFacts) {
+    TextView title = animalFacts.findViewById(R.id.title);
+    TextView facts = animalFacts.findViewById(R.id.facts);
+
+    if(scannedAnimal.equals("penguin")) {
+      title.setText("Penguin");
+      facts.setText(Animal.PENGUIN_FACTS);
+    } else if(scannedAnimal.equals("seahorse")) {
+      title.setText("Seahorse");
+      facts.setText(Animal.SEAHORSE_FACTS);
+    } else if(scannedAnimal.equals("dolphin")) {
+      title.setText("Dolphin");
+      facts.setText(Animal.DOLPHIN_FACTS);
+    } else if(scannedAnimal.equals("turtle")) {
+      title.setText("Turtle");
+      facts.setText(Animal.TURTLE_FACTS);
+    }
   }
 
   private String generateFilename() {
--- app/src/main/java/com/example/aquaticanimals/utils/Animal.java
+++ app/src/main/java/com/example/aquaticanimals/utils/Animal.java
@@ -8,6 +8,7 @@ public class Animal {
     public static final String PENGUIN_DIET = ": Carnivore";
     public static final String PENGUIN_HABITAT = ": Ocean, Coasts. Mostly in the Southern Hemisphere";
     public static final String PENGUIN_STATUS = ": Endangered";
+    public static final String PENGUIN_FACTS = "Penguins live in the southern hemisphere.\nPenguins are birds. They cannot fly but are known as the fastest swimming birds.\nTheir black and white colour provides good camouflage while they are in the water and protects them from predators.\nSimilar to birds, penguins lay eggs to reproduce.";
 
     public static final String SEAHORSE_ANIMAL_GROUP = ": Fish";
     public static final String SEAHORSE_SIZE = ": 2.5 - 35.6 cm";
@@ -16,6 +17,7 @@ public class Animal {
     public static final String SEAHORSE_DIET = ": Carnivore";
     public static final String SEAHORSE_HABITAT = ": Ocean";
     public static final String SEAHORSE_STATUS = ": Vulnerable";
+    public static final String SEAHORSE_FACTS = "Seahorses are a type of fish that get their name from their horse-like heads.\nSeahorses can quickly change colours to blend with the background. This camouflaging technique protects them from predators.\nSeahorses lay eggs to reproduce.";
 
     public static final String DOLPHIN_ANIMAL_GROUP = ": Mammal";
     public static final String DOLPHIN_SIZE = ": 152 - 914 cm";
@@ -24,6 +26,7 @@ public class Animal {
     public static final String DOLPHIN_DIET = ": Carnivore";
     public static final String DOLPHIN_HABITAT = ": Ocean, River";
     public static final String DOLPHIN_STATUS = ": Endangered";
+    public static final String DOLPHIN_FACTS = "Dolphins live in groups of pods.\nThey live underwater, but come up to the surface to breathe using the blowhole at the top of their head.\nAs with any mammals, dolphins give birth to reproduce.\nAn interesting fact is dolphins do not drink water, they simply get water from the fish they eat.";
 
     public static final String TURTLE_ANIMAL_GROUP = ": Reptile";
     public static final String TURTLE_SIZE = ": 61 - 183 cm";
@@ -32,4 +35,5 @@ public class Animal {
     public static final String TURTLE_DIET = ": Most are Carnivore";
     public static final String TURTLE_HABITAT = ": Ocean";
     public static final String TURTLE_STATUS = ": Endangered";
+    public static final String TURTLE_FACTS = "Turtles are one of the oldest reptile groups in the world.\nThey have a hard shell that protects them from predators.\nTurtles lay eggs to reproduce. These eggs are usually laid in the sand at beaches.";
 }
